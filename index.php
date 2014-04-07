1c191546b2b428b4fc8e4dc96aa65526b7f74ae8
inkej <inkeroinenjussi@gmail.com>
2014-04-07 10:41:25 +0300

    Testi

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Testi</title>
+	<link href="style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+	<div id="header">
+	</div>
+	
+	<div id="top">
+		<div id="top-container">
+			<h1>Welcome.</h1>
+		</div>
+	</div>
+	
+	<div id="main">
+	</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
